19e8fbf93c4268b8e4f3ca3ba6dfa54f59259171
achrefmechergui <achreffcb123achref@gamil.com>
2025-09-23 20:57:11 +0100

    pig game -done

--- app.js
+++ app.js
@@ -1,65 +1,106 @@
-"use strict"
+"use strict";
 
-///// import elements ::: 
-let score0 = document.querySelector(".score-0")
-let score1 = document.querySelector(".score-1")
+///// import elements :::
+let score0 = document.querySelector(".score-0");
+let score1 = document.querySelector(".score-1");
 
-let diceImg = document.querySelector("img")
-const rollBtn = document.querySelector(".roll")
-const holdBtn = document.querySelector(".hold")
-const player0 = document.querySelector(".player-0")
-const player1 = document.querySelector(".player-1")
+let diceImg = document.querySelector("img");
+const rollBtn = document.querySelector(".roll");
+const holdBtn = document.querySelector(".hold");
+const player0 = document.querySelector(".player-0");
+const player1 = document.querySelector(".player-1");
+const resetBtn = document.querySelector(".reset") 
 
-
-// ///// start condition :: 
-score0.textContent = 0 ; 
-score1.textContent = 0 ; 
-diceImg.classList.add("hidden")
-
-let score = 0 ;
-let activePlayer = 0 ;
-
-let fixedScores = [0,0]
+// ///// start condition ::
+// score0.textContent = 0;
+// score1.textContent = 0;
+// diceImg.classList.add("hidden");
 
 
-function switchPlayer (){
-   score = 0 ;
-         document.querySelector(`.current-score-${activePlayer}`).textContent = 0 
-         activePlayer = activePlayer === 0 ? 1 : 0
-        player0.classList.toggle("active")
-        player1.classList.toggle("active")
+
+// let isPlaying = true;
+
+
+// let score = 0;
+// let activePlayer = 0;
+
+// let fixedScores = [0, 0];
+
+let isPlaying , score , activePlayer , fixedScores 
+
+
+
+
+  function newGame (){   //// generick function type declaration
+  score0.textContent = 0;
+  score1.textContent = 0;
+  diceImg.classList.add("hidden");
+  isPlaying = true ; 
+  score = 0 ; 
+  activePlayer = 0 ; 
+  fixedScores = [0,0] ;
+  player0.classList.add("active")
+  player1.classList.remove("active")
+  player0.classList.remove("winner")
+  player1.classList.remove("winner")
+  document.querySelector(".current-score-0").textContent = 0
+   document.querySelector(".current-score-1").textContent = 0
+  }
+
+  ///// start condition
+  newGame()
+
+
+
+
+
+
+
+
+
+function switchPlayer() {
+  score = 0;
+  document.querySelector(`.current-score-${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle("active");
+  player1.classList.toggle("active");
 }
 
-///// roll functionnality ::: 
+///// roll functionnality :::
 
-rollBtn.addEventListener("click" ,function(){
-    const diceNumber = Math.trunc(Math.random()*6)+1  ///// 1 ===> 6
-    diceImg.src = `dice-${diceNumber}.png`
-    diceImg.classList.remove("hidden")
+rollBtn.addEventListener("click", function () {
+  if (isPlaying) {
+    const diceNumber = Math.trunc(Math.random() * 6) + 1; ///// 1 ===> 6
+    diceImg.src = `dice-${diceNumber}.png`;
+    diceImg.classList.remove("hidden");
 
-    if (diceNumber !==1) {
-    //  score = score + diceNumber
-    score+=diceNumber
-    document.querySelector(`.current-score-${activePlayer}`).textContent = score
+    if (diceNumber !== 1) {
+      //  score = score + diceNumber
+      score += diceNumber;
+      document.querySelector(`.current-score-${activePlayer}`).textContent =
+        score;
     } else {
-     switchPlayer()
+      switchPlayer();
     }
-  
-})
+  }
+});
+
+//// hold functionnality :::
+
+holdBtn.addEventListener("click", function () {
+  if (isPlaying) {
+    fixedScores[activePlayer] += score;
+    document.querySelector(`.score-${activePlayer}`).textContent =
+      fixedScores[activePlayer]; /// update ui
+
+    if (fixedScores[activePlayer] >= 20) {
+      isPlaying = false;
+      document.querySelector(`.player-${activePlayer}`).classList.add("winner");
+    } else {
+      switchPlayer();
+    }
+  }
+});
 
 
-//// hold functionnality ::: 
-
-holdBtn.addEventListener("click",function(){
-//  fixedScores[activePlayer] = fixedScores[activePlayer] + score ;
-fixedScores[activePlayer] += score
-document.querySelector(`.score-${activePlayer}`).textContent = fixedScores[activePlayer]
-
-if (fixedScores[activePlayer] >= 20){
-document.querySelector(`.player-${activePlayer}`).classList.add("winner")
-}
-
-else {
-   switchPlayer()
-}
-})
+resetBtn.addEventListener("click" , newGame)
